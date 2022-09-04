cd7db132c96811a41c56c024a7de9cc6600994e8
Brogan Fitzgerald <broganfitzy@gmail.com>
2022-09-04 15:18:36 +0200

    More Tings innit
    Its all not working.

--- pages/api/errorFromArticle.js
+++ pages/api/errorFromArticle.js
@@ -5,25 +5,25 @@ import mongoose from 'mongoose'
 
 export default function Handler(req, res) {
 
-    dbConnect()
-
-    return new Promise(resolve => {
-        if (req.method === 'POST') {
-            try{
-                let data = req.body
-                var id = mongoose.mongo.ObjectId(data)
-                Articles.deleteOne({ _id: id }).then((e) => {
-                    return res.status("200")
-                })
-                return res.status("500")
+    dbConnect().then(() => {
+        return new Promise(resolve => {
+            if (req.method === 'POST') {
+                try{
+                    let data = req.body
+                    var id = mongoose.mongo.ObjectId(data)
+                    Articles.deleteOne({ _id: id }).then((e) => {
+                        return res.status("200")
+                    })
+                    return res.status("500")
+                }
+                catch(e){
+                    return res.status("200").json(e)
+                }
             }
-            catch(e){
-                return res.status("200").json(e)
+            else {
+                res.redirect("/").end()
             }
-        }
-        else {
-            res.redirect("/").end()
-        }
+        })
     })
 
 }
--- pages/api/getCategories.js
+++ pages/api/getCategories.js
@@ -6,57 +6,56 @@ import mongoose from 'mongoose'
 
 export default function Handler(req, res) {
 
-    console.log(dbConnect())
-
-    return new Promise(resolve => {
-        if (req.method === 'POST') {
-            if(req.body == ""){
-                try{
-                    
-                    Category.find({}).then((resp) => {
-                        let tags = []
-                        for(let i = 0; i < resp.length; i++){
-                            tags.push({value: resp[i].category, label: resp[i].category})
-                        }
-                        tags.sort(function (a, b) {
-                            if (a.value < b.value) {
-                            return -1;
+    dbConnect().then(() => {
+        return new Promise(resolve => {
+            if (req.method === 'POST') {
+                if(req.body == ""){
+                    try{
+                        
+                        Category.find({}).then((resp) => {
+                            let tags = []
+                            for(let i = 0; i < resp.length; i++){
+                                tags.push({value: resp[i].category, label: resp[i].category})
                             }
-                            if (a.value > b.value) {
-                            return 1;
-                            }
-                            return 0;
+                            tags.sort(function (a, b) {
+                                if (a.value < b.value) {
+                                return -1;
+                                }
+                                if (a.value > b.value) {
+                                return 1;
+                                }
+                                return 0;
+                            })
+    
+                            return res.status("200").json(tags)
                         })
-
-                        return res.status("200").json(tags)
-                    })
-        
-                    return res.status("500")
+            
+                        return res.status("500")
+                    }
+                    catch(e){
+                        return res.status("200").json(e)
+                    }
                 }
-                catch(e){
-                    return res.status("200").json(e)
-                }
-            }
-            else if(req.body == "GetCategories"){
-                try{
-                    Articles.find({}).then((resp) => {
-                        let jsonData = []
-                        for(var i = 0; i < resp.length; i++){
-                            let id = resp[i]["_id"].toString()
-                            jsonData.push({"id": id, "tags": resp[i].categories})
-                        }
-                        return res.status("200").json(jsonData)
-                    })
-        
-                    return res.status("500")
-                }
-                catch(e){
-                    return res.status("200").json(e)
+                else if(req.body == "GetCategories"){
+                    try{
+                        Articles.find({}).then((resp) => {
+                            let jsonData = []
+                            for(var i = 0; i < resp.length; i++){
+                                let id = resp[i]["_id"].toString()
+                                jsonData.push({"id": id, "tags": resp[i].categories})
+                            }
+                            return res.status("200").json(jsonData)
+                        })
+            
+                        return res.status("500")
+                    }
+                    catch(e){
+                        return res.status("200").json(e)
+                    }
                 }
             }
-        }
+        })
     })
-
 }
 
 /*
