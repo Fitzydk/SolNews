ef9a5bce30189418b8ff088ac98670b46b34bb98
Brogan Fitzgerald <broganfitzy@gmail.com>
2022-09-10 18:51:35 +0200

    Update uploadArticle.js
    yrah

--- pages/api/uploadArticle.js
+++ pages/api/uploadArticle.js
@@ -4,33 +4,32 @@ import Articles from "../../models/Articles"
 
 export default function Handler(req, res) {
 
-    dbConnect()
-
-    return new Promise(resolve => {
-        if (req.method === 'POST') {
-            try{
-                let data = JSON.parse(req.body)
-                for(var i = 0; i < data.content.length; i++){
-                    if(data.content[i].type == "image"){
-                        let tempUrlFile = data.content[i].data.url
-                        delete data.content[i].data.url
-                        data.content[i].data.file = {url: tempUrlFile}
+    dbConnect().then(() => {
+        return new Promise(resolve => {
+            if (req.method === 'POST') {
+                try{
+                    let data = JSON.parse(req.body)
+                    for(var i = 0; i < data.content.length; i++){
+                        if(data.content[i].type == "image"){
+                            let tempUrlFile = data.content[i].data.url
+                            delete data.content[i].data.url
+                            data.content[i].data.file = {url: tempUrlFile}
+                        }
                     }
+                    var n = new Articles();
+                    n.address = data["wallet"]
+                    n.categories = data["tags"]
+                    n.save(function(err, article) {
+                       return res.status("201").json({id: article["_id"], content: data["content"]})
+                    })
                 }
-                var n = new Articles();
-                n.address = data["wallet"]
-                n.categories = data["tags"]
-                n.save(function(err, article) {
-                   return res.status("201").json({id: article["_id"], content: data["content"]})
-                })
+                catch(e){
+                    return res.status("201").json(e)
+                }
             }
-            catch(e){
-                return res.status("201").json(e)
+            else {
+                res.redirect("/").end()
             }
-        }
-        else {
-            res.redirect("/").end()
-        }
+        })
     })
-
 }
